e27c1c51445f64f94fb748b1b4edcb9287d1b0ea
sebastian-alejandro <sebastian.alejandro.hv@gmail.com>
2019-08-12 15:10:12 -0400

    default project

---
+++ src/main/java/com/sebastiansoftwareengineer/barbershopauthorizationserver/BarbershopAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiansoftwareengineer.barbershopauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbershopAuthorizationServerApplication.class, args);
+    }
+
+}
